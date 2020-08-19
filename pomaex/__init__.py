74e4f7edf642b88cf408783fd5d1d78db3a8a115
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-08-19 09:44:57 -0300

    Adding boilerplate / maintenance files

--- pomaex/__init__.py
+++ pomaex/__init__.py
@@ -1,1 +1,13 @@
-__version__ = "0.1.0"
+# type: ignore[attr-defined]
+"""Portable Market Exchange {Repository,Scrapper,Format} for Python"""
+
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:  # pragma: no cover
+    from importlib_metadata import version, PackageNotFoundError
+
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
